9b4f041feb8ded141446711677ce50da2fc74b06
pzom <pe.zombardi@gmail.com>
2026-05-29 18:54:27 -0300

    (feat): Added OutputStream callback

---
+++ core/midi-converter.py
@@ -0,0 +1,4 @@
+### Converts MIDI messages to its corresponding frequecy
+def convert_midi_to_frequency(msg):
+    frequency = 0
+    return frequency
--- main.py
+++ main.py
@@ -1,14 +1,49 @@
 import mido
+import numpy as np
+import sounddevice as sd
+import threading
+# Globals and Constants
+sample_rate = 44100.0
+active_notes = {}
+notes_lock = threading.Lock()
 
-input_ports = mido.get_input_names()
-target_port_name = input_ports[1]  # Hardcoded index for the desired MIDI input port
-print(f"Opening MIDI input port: {target_port_name}")
+def convert_midi_to_frequency(msg):
+    return 440 * 2 ** ((msg.note - 69) / 12)
 
-with mido.open_input(target_port_name) as in_port:
-    for msg in in_port:
-        print(msg)
+def audio_callback(outdata, frames, time, status):
+    if status:
+        print(status)
+    amplitude =0.2
+    mixed_signal = np.zeros(frames)
+    
+    with notes_lock:
+        for note_id, (freq, phase) in list(active_notes.items()):
+            t = (np.arange(frames) + phase) / sample_rate
+            
+            mixed_signal += amplitude * np.sin(2 * np.pi * freq * t)
+            
+            active_notes[note_id] = (freq, phase + frames)
+            
+    outdata[:] = mixed_signal.reshape(-1, 1)
 
-print("Available MIDI input ports:")
-for i, port in enumerate(input_ports):
-    print(f"{i}: {port}")   
+def main():
+    input_ports = mido.get_input_names()
+    target_port_name = input_ports[1]  
 
+    try:
+        with sd.OutputStream(samplerate=sample_rate, channels=1, callback=audio_callback):
+            with mido.open_input(target_port_name) as in_port:
+                for msg in in_port:
+                    if msg.type == 'note_on' and msg.velocity > 0:
+                        freq = convert_midi_to_frequency(msg)
+                        with notes_lock:
+                            active_notes[msg.note] = (freq, 0)
+                    
+                    elif msg.type == 'note_off' or (msg.type == 'note_on' and msg.velocity == 0):
+                        with notes_lock:
+                            if msg.note in active_notes:
+                                del active_notes[msg.note]
+
+    except KeyboardInterrupt:
+        print("Exiting...")
+main()
