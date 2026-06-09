c1e3a48a9583a482e6c21c2667b8d70fa86a1b69
AndreDRI <andredri@proton.me>
2026-06-09 15:36:50 -0300

    (feat): Basic GUI interface

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello, MIDI World!")
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+root.title("Hello World")
+root.geometry("400x200")
+label = ttk.Label(root, text="Hello World!", font=("Arial", 16))
+label.pack(expand=True)
+root.mainloop()
+
+
